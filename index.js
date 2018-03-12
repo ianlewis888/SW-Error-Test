0ff9952ac8536361946e8b28c447bbf418c3774d
Ian Lewis <ian.lewis748@gmail.com>
2018-03-12 13:32:02 -0400

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 80;
 app.get('/Results.aspx', (req, res) => {
   const errMessage = JSON.stringify({"ErrorReturningXML": "true"});
   res.append("Content-Type", "application/json;charSet=UTF-8");
+  res.append("Access-Control-Allow-Origin", "*");
   res.send(errMessage);
 });
 
