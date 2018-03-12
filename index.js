7603b0f55b12a76f7aaa30b803a9f37c8867d53f
Ian Lewis <ian.lewis748@gmail.com>
2018-03-12 13:33:34 -0400

    allow origin "*.netmng.com"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 80;
 app.get('/Results.aspx', (req, res) => {
   const errMessage = JSON.stringify({"ErrorReturningXML": "true"});
   res.append("Content-Type", "application/json;charSet=UTF-8");
-  res.append("Access-Control-Allow-Origin", "*");
+  res.append("Access-Control-Allow-Origin", "*.netmng.com");
   res.send(errMessage);
 });
 
